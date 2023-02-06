4c9db30ce7b5174f546a4bca020a292ddbef1870
“Alex” <498513334@qq.com>
2023-02-06 09:57:14 +0800

    init: 🎉

--- src/App.vue
+++ src/App.vue
@@ -10,16 +10,25 @@ const router = useRouter();
     <div class="relative flex-1 flex items-center justify-center">
       <router-view></router-view>
       <div
-        class="absolute cursor-pointer w-40px h-40px bg-white flex items-center justify-center border-rd-100% right-15px bottom-15px shadow"
-      >
+        class="absolute cursor-pointer w-40px h-40px bg-white flex items-center justify-center border-rd-100% right-15px bottom-15px shadow">
         <div class="i-ic-baseline-code text-22px text-#444"></div>
       </div>
     </div>
     <div
       class="fixed right-10px top-10px cursor-pointer w-40px h-40px bg-white flex items-center justify-center border-rd-100% right-15px bottom-15px shadow"
-      @click="router.push('/')"
-    >
+      @click="router.push('/')">
       <div class="i-ic-round-home text-28px text-#444"></div>
     </div>
   </div>
 </template>
+
+<style>
+body,
+html {
+  margin: 0;
+}
+
+canvas {
+  display: block;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
+import { router } from "./router";
 import "./style.css";
 import "uno.css";
-import { router } from "./router";
 import App from "./App.vue";
 
 createApp(App).use(router).mount("#app");
